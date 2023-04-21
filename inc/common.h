e4240f639e147a5e899bf6cbf890c9a931ef1547
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-04-21 15:25:47 -0700

    udpate common.h

--- inc/common.h
+++ inc/common.h
@@ -48,15 +48,7 @@ using MPI_Count_type = int;
 using MPI_Displ_type = int;
 
 #define MPI_COUNT_TYPE MPI_INT
-
-#define MPI_ALLTOALL MPI_Alltoall
-#define MPI_ALLTOALLV MPI_Alltoallv
-#define MPI_SCATTER MPI_Scatter
-#define MPI_SCATTERV MPI_Scatterv
-#define MPI_GATHER MPI_Gather
-#define MPI_GATHERV MPI_Gatherv
-#define MPI_BCAST MPI_Bcast
-#define MPI_FILE_READ_AT_ALL MPI_File_read_at_all
+#define MPI_FUNC_SELECT(funcname) funcname
 
 #elif MPI_VERSION == 4
 #define MPI_HAS_LARGE_COUNTS 1
@@ -65,19 +57,20 @@ using MPI_Count_type = MPI_Count;
 using MPI_Displ_type = MPI_Aint;
 
 #define MPI_COUNT_TYPE MPI_COUNT
-
-#define MPI_ALLTOALL MPI_Alltoall_c
-#define MPI_ALLTOALLV MPI_Alltoallv_c
-#define MPI_SCATTER MPI_Scatter_c
-#define MPI_SCATTERV MPI_Scatterv_c
-#define MPI_GATHER MPI_Gather_c
-#define MPI_GATHERV MPI_Gatherv_c
-#define MPI_BCAST MPI_Bcast_c
-#define MPI_FILE_READ_AT_ALL MPI_File_read_at_all_c
+#define MPI_FUNC_SELECT(funcname) funcname##_c
 
 #else
 #error "MPI version should either be 3 or 4."
 #endif
+
+#define MPI_ALLTOALL         MPI_FUNC_SELECT(MPI_Alltoall)
+#define MPI_ALLTOALLV        MPI_FUNC_SELECT(MPI_Alltoallv)
+#define MPI_SCATTER          MPI_FUNC_SELECT(MPI_Scatter)
+#define MPI_SCATTERV         MPI_FUNC_SELECT(MPI_Scatterv)
+#define MPI_GATHER           MPI_FUNC_SELECT(MPI_Gather)
+#define MPI_GATHERV          MPI_FUNC_SELECT(MPI_Gatherv)
+#define MPI_BCAST            MPI_FUNC_SELECT(MPI_Bcast)
+#define MPI_FILE_READ_AT_ALL MPI_FUNC_SELECT(MPI_File_read_at_all)
 
 #ifndef MPI_SIZE_T
 static_assert(std::numeric_limits<size_t>::max() == std::numeric_limits<unsigned long>::max());
