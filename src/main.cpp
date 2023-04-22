b20d9582adf4e9ae8b4031e98f18f84cf3e06423
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-04-22 14:11:20 -0700

    parse_cli

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <cstdint>
 #include <cassert>
+#include <cstdio>
 #include <numeric>
 #include <unistd.h>
 #include <mpi.h>
@@ -15,95 +16,50 @@ int returncode;
 String fasta_fname;
 
 /*
+ * MPI communicator info.
+ */
+MPI_Comm comm;
+int myrank;
+int nprocs;
+
+/*
  * X-Drop alignment parameters.
  */
-int mat = 1;
-int mis = -1;
-int gap = -1;
-int xdrop_cutoff = 15;
+int mat = 1;           /* match score */
+int mis = -1;          /* mismatch score */
+int gap = -1;          /* gap score */
+int xdrop_cutoff = 15; /* x-drop cutoff score */
 
-constexpr int root = 0;
+constexpr int root = 0; /* root process rank */
 
-void usage(char const *prg);
+/*
+ * Timer variable(s).
+ */
+double elapsed, mintime, maxtime, avgtime;
+
+int parse_cli(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
 
     {
-        MPI_Comm comm = MPI_COMM_WORLD;
-        auto commgrid = Grid(new CommGrid(comm, 0, 0));
-        int myrank = commgrid->GetRank();
-        int nprocs = commgrid->GetSize();
-        int params[4] = {mat, mis, gap, xdrop_cutoff};
-        int show_help = 0;
+        /*
+         * CombBLAS 2D processor grid uses MPI_COMM_WORLD
+         * as its world communicator (comm).
+         */
+        Grid commgrid;
 
-        if (myrank == root)
-        {
-            int c;
+        comm = MPI_COMM_WORLD;
+        commgrid.reset(new CommGrid(comm, 0, 0));
+        myrank = commgrid->GetRank();
+        nprocs = commgrid->GetSize();
 
-            while ((c = getopt(argc, argv, "x:A:B:G:h")) >= 0)
-            {
-                if      (c == 'A') params[0] =  atoi(optarg);
-                else if (c == 'B') params[1] = -atoi(optarg);
-                else if (c == 'G') params[2] = -atoi(optarg);
-                else if (c == 'x') params[3] =  atoi(optarg);
-                else if (c == 'h') show_help = 1;
-            }
-        }
-
-        MPI_BCAST(params, 4, MPI_INT, root, comm);
-
-        mat          = params[0];
-        mis          = params[1];
-        gap          = params[2];
-        xdrop_cutoff = params[3];
-
-        if (myrank == root && show_help)
-            usage(argv[0]);
-
-        MPI_BCAST(&show_help, 1, MPI_INT, root, comm);
-        if (show_help) goto err;
-
-        int fasta_provided = 1;
-        if (myrank == root && optind + 1 != argc)
-        {
-            std::cerr << "error: missing FASTA file" << std::endl;
-            usage(argv[0]);
-            fasta_provided = 0;
-        }
-
-        MPI_BCAST(&fasta_provided, 1, MPI_INT, root, comm);
-        if (!fasta_provided) goto err;
-
-        int fasta_fname_len;
-        if (myrank == root)
-        {
-            fasta_fname = argv[optind];
-            fasta_fname_len = fasta_fname.size();
-
-            std::cout << "-DKMER_SIZE="       << KMER_SIZE       << "\n"
-                      << "-DLOWER_KMER_FREQ=" << LOWER_KMER_FREQ << "\n"
-                      << "-DUPPER_KMER_FREQ=" << UPPER_KMER_FREQ << "\n"
-            #ifdef USE_BLOOM
-                      << "-DUSE_BLOOM\n"
-            #endif
-                      << "\n"
-                      << "int mat = "          << mat          <<   ";\n"
-                      << "int mis = "          << mis          <<   ";\n"
-                      << "int gap = "          << gap          <<   ";\n"
-                      << "int xdrop_cutoff = " << xdrop_cutoff <<   ";\n"
-                      << "String fname = \""   << fasta_fname  << "\";\n"
-                      << std::endl;
-        }
-
-        MPI_BCAST(&fasta_fname_len, 1, MPI_INT, root, comm);
-
-        if (myrank != root) fasta_fname.assign(fasta_fname_len, '\0');
-
-        MPI_BCAST(fasta_fname.data(), fasta_fname_len, MPI_CHAR, root, comm);
-
-        double elapsed, mintime, maxtime, avgtime;
+        /*
+         * Parse command-line from root process and broadcast.
+         */
+        if (parse_cli(argc, argv) != 0)
+            goto err;
 
         MPI_Barrier(comm);
         elapsed = MPI_Wtime();
@@ -142,4 +98,77 @@ void usage(char const *prg)
               << "         -G INT   gap penalty ["                << -gap          << "]\n"
               << "         -h       help message"
               << std::endl;
+}
+
+int parse_cli(int argc, char *argv[])
+{
+    int params[4] = {mat, mis, gap, xdrop_cutoff};
+    int show_help = 0, fasta_provided = 1;
+
+    if (myrank == root)
+    {
+        int c;
+
+        while ((c = getopt(argc, argv, "x:A:B:G:h")) >= 0)
+        {
+            if      (c == 'A') params[0] =  atoi(optarg);
+            else if (c == 'B') params[1] = -atoi(optarg);
+            else if (c == 'G') params[2] = -atoi(optarg);
+            else if (c == 'x') params[3] =  atoi(optarg);
+            else if (c == 'h') show_help = 1;
+        }
+    }
+
+    MPI_BCAST(params, 4, MPI_INT, root, comm);
+
+    mat          = params[0];
+    mis          = params[1];
+    gap          = params[2];
+    xdrop_cutoff = params[3];
+
+    if (myrank == root && show_help)
+        usage(argv[0]);
+
+    MPI_BCAST(&show_help, 1, MPI_INT, root, comm);
+    if (show_help) return -1;
+
+    if (myrank == root && optind >= argc)
+    {
+        std::cerr << "error: missing FASTA file\n";
+        usage(argv[0]);
+        fasta_provided = 0;
+    }
+
+    MPI_BCAST(&fasta_provided, 1, MPI_INT, root, comm);
+    if (!fasta_provided) return -1;
+
+    int fnamelen;
+
+    if (myrank == root)
+    {
+        fasta_fname = argv[optind];
+        fnamelen = fasta_fname.size();
+
+        std::cout << "-DKMER_SIZE="       << KMER_SIZE       << "\n"
+                  << "-DLOWER_KMER_FREQ=" << LOWER_KMER_FREQ << "\n"
+                  << "-DUPPER_KMER_FREQ=" << UPPER_KMER_FREQ << "\n"
+        #ifdef USE_BLOOM
+                  << "-DUSE_BLOOM\n"
+        #endif
+                  << "\n"
+                  << "int mat = "          << mat          <<   ";\n"
+                  << "int mis = "          << mis          <<   ";\n"
+                  << "int gap = "          << gap          <<   ";\n"
+                  << "int xdrop_cutoff = " << xdrop_cutoff <<   ";\n"
+                  << "String fname = \""   << fasta_fname  << "\";\n"
+                  << std::endl;
+    }
+
+    MPI_BCAST(&fnamelen, 1, MPI_INT, root, comm);
+
+    if (myrank != root) fasta_fname.assign(fnamelen, '\0');
+
+    MPI_BCAST(fasta_fname.data(), fnamelen, MPI_CHAR, root, comm);
+
+    return 0;
 }
